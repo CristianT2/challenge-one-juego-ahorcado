4491078238cdd274582a4fa75d2ddb98164fc853
Cristian Torrejon <c.torrejon96@gmail.com>
2022-09-30 18:11:14 -0300

    Se subio un cambio

--- JavaScript/palabras.js
+++ JavaScript/palabras.js
@@ -1,6 +1,6 @@
-var listaPalabras = ["PYTHON","CORAZON","JAVA","MARTE","AGUA","ESPEJO","MUSICA",
-                    "CODIGO","EUROPA","ESPADA","SOL","GORRA","CELULAR","HELADO",
-                    "AMERICA","FUTBOL","ALURA","LATAM","TREBOL","LUNA","AFRICA",
+var listaPalabras = ["PYTHON","PLANETA","JAVA","MARTE","AGUA","ESPEJO","MUSICA",
+                    "CODIGO","EUROPA","ORACLE","SOL","GORRA","CELULAR","HELADO",
+                    "AMERICA","FUTBOL","ALURA","LATAM","YOUTUBE","LUNA","AFRICA",
                     "HANDBALL","LLUVIA","BANDERA","MATE","AVION","BACKEND","NIEVE",
                     "TIEMPO","FACEBOOK","GOOGLE","LINUX","ANDROID","VEHICULO","CIELO"];
 
